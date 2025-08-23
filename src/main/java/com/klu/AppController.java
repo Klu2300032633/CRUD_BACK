337ce9d76bc55b864fcfd2b963f820d43bee1b12
Klu2300032633 <2300032633@kluniversity.in>
2025-08-24 01:41:57 +0530

    backend v3.0

--- src/main/java/com/klu/AppController.java
+++ src/main/java/com/klu/AppController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:5173")
 public class AppController {
 	@Autowired
 	Service s;
